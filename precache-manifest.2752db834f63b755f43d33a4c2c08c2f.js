323045f5e22b67787727fbc1de508703c7676689
Saad Mecci <saadmecci@yahoo.com>
2020-03-01 18:59:36 -0600

    Updates

---
+++ precache-manifest.2752db834f63b755f43d33a4c2c08c2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "377cb50bcc2b5f53f41596d3dd480a7a",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "7aba83832196898afe2c",
+    "url": "/shopping-cart/static/css/2.5353b2f2.chunk.css"
+  },
+  {
+    "revision": "12c4ac9c1cb697c8fc86",
+    "url": "/shopping-cart/static/css/main.6d67975a.chunk.css"
+  },
+  {
+    "revision": "7aba83832196898afe2c",
+    "url": "/shopping-cart/static/js/2.5a504ec4.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/shopping-cart/static/js/2.5a504ec4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "12c4ac9c1cb697c8fc86",
+    "url": "/shopping-cart/static/js/main.d751a85c.chunk.js"
+  },
+  {
+    "revision": "99ac323b0c111b035cb3",
+    "url": "/shopping-cart/static/js/runtime-main.916889a6.js"
+  }
+]);
